677effe249741ed2b178a05ff94556fb87672a3c
AntoBrandi <antonio.brandi@outlook.it>
2022-09-19 22:17:29 +0200

    added odometry model

--- bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.h
+++ bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.h
@@ -3,6 +3,8 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <Eigen/Core>
+#include <sensor_msgs/JointState.h>
+#include <nav_msgs/Odometry.h>
 
 
 class SimpleController
@@ -13,12 +15,20 @@ public:
 private:
     void velCallback(const geometry_msgs::Twist &);
 
+    void jointCallback(const sensor_msgs::JointState &);
+
     ros::NodeHandle nh_;
     ros::Subscriber vel_sub_;
     ros::Publisher right_cmd_pub_;
     ros::Publisher left_cmd_pub_;
+    ros::Subscriber joint_sub_;
+    ros::Publisher odom_pub_;
 
     double wheel_radius_;
     double wheel_separation_;
     Eigen::Matrix2d speed_conversion_;
+    double right_wheel_prev_pos_;
+    double left_wheel_prev_pos_;
+    ros::Time prev_time_;
+    nav_msgs::Odometry odom_msg_;
 };
--- bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
+++ bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
@@ -8,14 +8,31 @@ SimpleController::SimpleController(const ros::NodeHandle &nh,
                                    double separation)
                                   : nh_(nh),
                                     wheel_radius_(radius),
-                                    wheel_separation_(separation)
+                                    wheel_separation_(separation),
+                                    left_wheel_prev_pos_(0.0),
+                                    right_wheel_prev_pos_(0.0)
 {
     right_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_right_controller/command", 10);
     left_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_left_controller/command", 10);
     vel_sub_ = nh_.subscribe("cmd_vel", 1000, &SimpleController::velCallback, this);
+    joint_sub_ = nh_.subscribe("joint_states", 1000, &SimpleController::jointCallback, this);
+    odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 10);
 
     speed_conversion_ << wheel_radius_/2, wheel_radius_/2, wheel_radius_/wheel_separation_, -wheel_radius_/wheel_separation_;
     ROS_INFO_STREAM("The conversion matrix is \n" << speed_conversion_);
+
+    odom_msg_.header.frame_id = "odom";
+    odom_msg_.child_frame_id = "base_footprint";
+    odom_msg_.twist.twist.linear.y = 0.0;
+    odom_msg_.twist.twist.linear.z = 0.0;
+    odom_msg_.twist.twist.angular.x = 0.0;
+    odom_msg_.twist.twist.angular.y = 0.0;
+    odom_msg_.pose.pose.orientation.x = 0.0;
+    odom_msg_.pose.pose.orientation.y = 0.0;
+    odom_msg_.pose.pose.orientation.z = 0.0;
+    odom_msg_.pose.pose.orientation.w = 1.0;
+
+    prev_time_ = ros::Time::now();
 }
 
 
@@ -35,4 +52,36 @@ void SimpleController::velCallback(const geometry_msgs::Twist &msg)
 
     right_cmd_pub_.publish(right_speed);
     left_cmd_pub_.publish(left_speed);
+}
+
+
+void SimpleController::jointCallback(const sensor_msgs::JointState &state)
+{
+    // Implements the inverse differential kinematic model
+    // Given the position of the wheels, calculates their velocities
+    // then calculates the velocity of the robot wrt the robot frame
+    // and then converts it in the global frame and publishes the TF
+    double dp_left = state.position.at(0) - left_wheel_prev_pos_;
+    double dp_right = state.position.at(1) - right_wheel_prev_pos_;
+    double dt = (state.header.stamp - prev_time_).toSec();
+
+    // Actualize the prev pose for the next itheration
+    left_wheel_prev_pos_ = state.position.at(0);
+    right_wheel_prev_pos_ = state.position.at(1);
+    prev_time_ = state.header.stamp;
+
+    // Calculate the rotational speed of each wheel
+    double fi_left = dp_left / dt;
+    double fi_right = dp_right / dt;
+
+    // Calculate the linear and angular velocity
+    double linear = (wheel_radius_ * fi_right + wheel_radius_ * fi_left) / 2;
+    double angular = (wheel_radius_ * fi_right - wheel_radius_ * fi_left) / wheel_separation_;
+
+    // Compose and publish the odom message
+    odom_msg_.header.stamp = ros::Time::now();
+    odom_msg_.twist.twist.linear.x = linear;
+    odom_msg_.twist.twist.angular.z = angular;
+    odom_pub_.publish(odom_msg_);
+    
 }
