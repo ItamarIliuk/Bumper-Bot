1dcd37dd1ee461b50f73889e255b3213ec242759
AntoBrandi <antonio.brandi@outlook.it>
2022-09-25 16:26:25 +0200

    added quaternion examples

--- bumperbot_ws/src/bumperbot_examples/include/bumperbot_examples/simple_tf_examples.h
+++ bumperbot_ws/src/bumperbot_examples/include/bumperbot_examples/simple_tf_examples.h
@@ -2,12 +2,18 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include "bumperbot_examples/GetTransform.h"
+#include <tf2/LinearMath/Quaternion.h>
 
 
 class SimpleTfExamples
 {
 public:
     SimpleTfExamples(const ros::NodeHandle &);
+
+    bool getTransformCallback(bumperbot_examples::GetTransform::Request &,
+                              bumperbot_examples::GetTransform::Response &);
 
 private: 
     void timerCallback(const ros::TimerEvent &);
@@ -16,5 +22,10 @@ private:
     geometry_msgs::TransformStamped static_transform_stamped_;
     geometry_msgs::TransformStamped dynamic_transform_stamped_;
     ros::Timer timer_;
-    double counter_;
+    double x_counter_;
+    ros::ServiceServer get_transform_srv_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
+    tf2::Quaternion last_orientation_;
+    tf2::Quaternion orientation_increment_;
 };
