7bc9d061734db8090f07da55d0db53e35a88e2c3
AntoBrandi <antonio.brandi@outlook.it>
2023-10-21 14:59:14 +0200

    uniform with course lessons

--- bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
+++ bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
@@ -1,11 +1,8 @@
-import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
-from ament_index_python.packages import get_package_share_directory
 
 
 def noisy_controller(context, *args, **kwargs):
@@ -42,10 +39,6 @@ def noisy_controller(context, *args, **kwargs):
 
 def generate_launch_description():
     
-    is_sim_arg = DeclareLaunchArgument(
-        "is_sim",
-        default_value="True",
-    )
     use_simple_controller_arg = DeclareLaunchArgument(
         "use_simple_controller",
         default_value="True",
@@ -71,48 +64,10 @@ def generate_launch_description():
         default_value="0.02",
     )
     
-    is_sim = LaunchConfiguration("is_sim")
     use_simple_controller = LaunchConfiguration("use_simple_controller")
     use_python = LaunchConfiguration("use_python")
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
-
-    robot_description = ParameterValue(
-        Command(
-            [
-                "xacro ",
-                os.path.join(
-                    get_package_share_directory("bumperbot_description"),
-                    "urdf",
-                    "bumperbot.urdf.xacro",
-                ),
-                " is_sim:=False"
-            ]
-        ),
-        value_type=str,
-    )
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}],
-        condition=UnlessCondition(is_sim),
-    )
-
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            {"robot_description": robot_description,
-             "use_sim_time": is_sim},
-            os.path.join(
-                get_package_share_directory("bumperbot_controller"),
-                "config",
-                "bumperbot_controllers.yaml",
-            ),
-        ],
-        condition=UnlessCondition(is_sim),
-    )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -168,15 +123,12 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            is_sim_arg,
             use_simple_controller_arg,
             use_python_arg,
             wheel_radius_arg,
             wheel_separation_arg,
             wheel_radius_error_arg,
             wheel_separation_error_arg,
-            robot_state_publisher_node,
-            controller_manager,
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
             simple_controller,
