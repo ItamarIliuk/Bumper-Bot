71a698544b91e697c1e0a146ceb24558a23b48ec
AntoBrandi <antonio.brandi@outlook.it>
2023-11-18 23:55:06 +0100

    using arduino nano and PID controller

--- bumperbot_ws/src/bumperbot_firmware/firmware/robot_control/robot_control.ino
+++ bumperbot_ws/src/bumperbot_firmware/firmware/robot_control/robot_control.ino
@@ -1,40 +1,54 @@
+#include <PID_v1.h>
+
 // L298N H-Bridge Connection PINs
-#define L298N_enA 25  // PWM
-#define L298N_enB 26  // PWM
-#define L298N_in4 19  // Dir Motor B
-#define L298N_in3 4  // Dir Motor B
-#define L298N_in2 32  // Dir Motor A
-#define L298N_in1 33  // Dir Motor A
+#define L298N_enA 9  // PWM
+#define L298N_enB 11  // PWM
+#define L298N_in4 8  // Dir Motor B
+#define L298N_in3 7  // Dir Motor B
+#define L298N_in2 13  // Dir Motor A
+#define L298N_in1 12  // Dir Motor A
 
 // Wheel Encoders Connection PINs
-#define right_encoder_phaseA 0  // Interrupt
-#define right_encoder_phaseB 13  // Interrupt
-#define left_encoder_phaseA 27   // Interrupt
-#define left_encoder_phaseB 14   // Interrupt
-
-// Setting PWM properties
-const int freq = 30000;
-const int ch_A = 0;
-const int ch_B = 1;
-const int resolution = 8;
+#define right_encoder_phaseA 3  // Interrupt 
+#define right_encoder_phaseB 5  
+#define left_encoder_phaseA 2   // Interrupt
+#define left_encoder_phaseB 4
 
 // Encoders
-unsigned int right_encoder_A_counter = 0;
-unsigned int right_encoder_B_counter = 0;
-unsigned int left_encoder_A_counter = 0;
-unsigned int left_encoder_B_counter = 0;
+unsigned int right_encoder_counter = 0;
+unsigned int left_encoder_counter = 0;
 unsigned long last_millis = 0;
 const unsigned long interval = 100;
 
+// Interpret Serial Messages
 bool is_right_wheel_cmd = false;
 bool is_left_wheel_cmd = false;
 bool is_right_wheel_forward = true;
 bool is_left_wheel_forward = true;
-float right_wheel_cmd_vel = 0.0; // rad/s
-float left_wheel_cmd_vel = 0.0;  // rad/s
-char value[] = "000";
+char value[] = "00.00";
 uint8_t value_idx = 0;
+bool is_cmd_complete = false;
 
+// PID
+// Setpoint - Desired
+double right_wheel_cmd_vel = 0.0;     // rad/s
+double left_wheel_cmd_vel = 0.0;      // rad/s
+// Input - Measurement
+double right_wheel_meas_vel = 0.0;    // rad/s
+double left_wheel_meas_vel = 0.0;     // rad/s
+// Output - Command
+double right_wheel_cmd = 0.0;             // 0-255
+double left_wheel_cmd = 0.0;              // 0-255
+// Tuning
+double Kp_r = 11.5;
+double Ki_r = 7.5;
+double Kd_r = 0.1;
+double Kp_l = 12.8;
+double Ki_l = 8.3;
+double Kd_l = 0.1;
+// Controller
+PID rightMotor(&right_wheel_meas_vel, &right_wheel_cmd, &right_wheel_cmd_vel, Kp_r, Ki_r, Kd_r, DIRECT);
+PID leftMotor(&left_wheel_meas_vel, &left_wheel_cmd, &left_wheel_cmd_vel, Kp_l, Ki_l, Kd_l, DIRECT);
 
 void setup() {
   // Init L298N H-Bridge Connection PINs
@@ -45,27 +59,19 @@ void setup() {
   pinMode(L298N_in3, OUTPUT);
   pinMode(L298N_in4, OUTPUT);
 
-  // configure LED PWM functionalitites
-  ledcSetup(ch_A, freq, resolution);
-  ledcSetup(ch_B, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(L298N_enA, ch_A);
-  ledcAttachPin(L298N_enB, ch_B);
-
   // Set Motor Rotation Direction
   digitalWrite(L298N_in1, HIGH);
   digitalWrite(L298N_in2, LOW);
   digitalWrite(L298N_in3, HIGH);
   digitalWrite(L298N_in4, LOW);
 
+  rightMotor.SetMode(AUTOMATIC);
+  leftMotor.SetMode(AUTOMATIC);
   Serial.begin(115200);
 
   // Set Callback for Wheel Encoders Pulse
-  attachInterrupt(digitalPinToInterrupt(right_encoder_phaseA), rightEncoderACallback, RISING);
-  attachInterrupt(digitalPinToInterrupt(right_encoder_phaseB), rightEncoderBCallback, RISING);
-  attachInterrupt(digitalPinToInterrupt(left_encoder_phaseA), leftEncoderACallback, RISING);
-  attachInterrupt(digitalPinToInterrupt(left_encoder_phaseB), leftEncoderBCallback, RISING);
+  attachInterrupt(digitalPinToInterrupt(right_encoder_phaseA), rightEncoderCallback, RISING);
+  attachInterrupt(digitalPinToInterrupt(left_encoder_phaseA), leftEncoderCallback, RISING);
 }
 
 void loop() {
@@ -79,8 +85,9 @@ void loop() {
       is_right_wheel_cmd = true;
       is_left_wheel_cmd = false;
       value_idx = 0;
+      is_cmd_complete = false;
     }
-    // Left Wheel Motor
+    // Left Wheel Mo tor
     else if(chr == 'l')
     {
       is_right_wheel_cmd = false;
@@ -128,23 +135,26 @@ void loop() {
     {
       if(is_right_wheel_cmd)
       {
-        right_wheel_cmd_vel = atoi(value);
+        right_wheel_cmd_vel = atof(value);
       }
       else if(is_left_wheel_cmd)
       {
-        left_wheel_cmd_vel = atoi(value);
+        left_wheel_cmd_vel = atof(value);
+        is_cmd_complete = true;
       }
       // Reset for next command
       value_idx = 0;
       value[0] = '0';
       value[1] = '0';
-      value[2] = '0';
-      value[3] = '\0';
+      value[2] = '.';
+      value[3] = '0';
+      value[4] = '0';
+      value[5] = '\0';
     }
     // Command Value
     else
     {
-      if(value_idx < 3)
+      if(value_idx < 5)
       {
         value[value_idx] = chr;
         value_idx++;
@@ -156,37 +166,41 @@ void loop() {
   unsigned long current_millis = millis();
   if(current_millis - last_millis >= interval)
   {
-    String encoder_read = "ra" + String(right_encoder_A_counter) + ",rb" + String(right_encoder_B_counter) + ",la" + String(left_encoder_A_counter) + ",lb" + String(left_encoder_B_counter) + ",";
+    right_wheel_meas_vel = (10 * right_encoder_counter * (60.0/385.0)) * 0.10472;
+    left_wheel_meas_vel = (10 * left_encoder_counter * (60.0/385.0)) * 0.10472;
+    
+    rightMotor.Compute();
+    leftMotor.Compute();
+
+    // Ignore commands smaller than inertia
+    if(right_wheel_cmd <= 60.0 && right_wheel_cmd_vel == 0.0)
+    {
+      right_wheel_cmd = 0.0;
+    }
+    if(left_wheel_cmd <= 60.0 && left_wheel_cmd_vel == 0.0)
+    {
+      left_wheel_cmd = 0.0;
+    }
+
+    String encoder_read = "r" + String(right_wheel_cmd) + ",l" + String(left_wheel_cmd) + ",";
     Serial.println(encoder_read);
     last_millis = current_millis;
-    right_encoder_A_counter = 0;
-    right_encoder_B_counter = 0;
-    left_encoder_A_counter = 0;
-    left_encoder_B_counter = 0;
-  }
+    right_encoder_counter = 0;
+    left_encoder_counter = 0;
 
-  ledcWrite(ch_A, right_wheel_cmd_vel);
-  ledcWrite(ch_B, left_wheel_cmd_vel);
+    analogWrite(L298N_enA, right_wheel_cmd);
+    analogWrite(L298N_enB, left_wheel_cmd);
+  }
 }
 
 // New pulse from Right Wheel Encoder
-void rightEncoderACallback()
+void rightEncoderCallback()
 {
-  right_encoder_A_counter++;
-}
-
-void rightEncoderBCallback()
-{
-  right_encoder_B_counter++;
+  right_encoder_counter++;
 }
 
 // New pulse from Left Wheel Encoder
-void leftEncoderACallback()
+void leftEncoderCallback()
 {
-  left_encoder_A_counter++;
-}
-
-void leftEncoderBCallback()
-{
-  left_encoder_B_counter++;
+  left_encoder_counter++;
 }
