c0b43aebfd46153c7a7073c3dcb27a3f08657560
AntoBrandi <antonio.brandi@outlook.it>
2023-12-24 11:22:51 +0100

    visualize markers zones

--- bumperbot_ws/src/bumperbot_utils/src/safety_stop.cpp
+++ bumperbot_ws/src/bumperbot_utils/src/safety_stop.cpp
@@ -8,6 +8,7 @@
 #include "std_msgs/msg/bool.hpp"
 #include "twist_mux_msgs/action/joy_turbo.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -21,7 +22,7 @@ enum State{
 class SafetyStop : public rclcpp::Node
 {
 public:
-  SafetyStop() : Node("safety_stop_node"), state_{State::FREE}, prev_state_{State::FREE}
+  SafetyStop() : Node("safety_stop_node"), is_first_msg_(true), state_{State::FREE}, prev_state_{State::FREE}
   {
     declare_parameter<double>("warning_distance", 0.6);
     declare_parameter<double>("danger_distance", 0.2);
@@ -35,6 +36,7 @@ public:
     laser_sub_ = create_subscription<sensor_msgs::msg::LaserScan>(
         scan_topic, 10, std::bind(&SafetyStop::laserCallback, this, _1));
     safety_stop_pub_ = create_publisher<std_msgs::msg::Bool>(safety_stop_topic, 10);
+    zones_pub_ = create_publisher<visualization_msgs::msg::MarkerArray>("zones", 10);
     decrease_speed_client_ = rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_decrease");
     increase_speed_client_ = rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_increase");
     while(!decrease_speed_client_->wait_for_action_server(1s) && rclcpp::ok())
@@ -47,13 +49,40 @@ public:
       RCLCPP_WARN(get_logger(), "Action /joy_turbo_increase not available! Waiting..");
       std::this_thread::sleep_for(2s);
     }
+
+    // Prepare Zones for Visualization
+    visualization_msgs::msg::Marker warning_zone;
+    warning_zone.id = 0;
+    warning_zone.type = visualization_msgs::msg::Marker::CYLINDER;
+    warning_zone.action = visualization_msgs::msg::Marker::ADD;
+    warning_zone.scale.z = 0.001;
+    warning_zone.scale.x = warning_distance_ * 2;
+    warning_zone.scale.y = warning_distance_ * 2;
+    warning_zone.color.r = 1.0;
+    warning_zone.color.g = 0.984;
+    warning_zone.color.b = 0.0;
+    warning_zone.color.a = 0.5;
+    zones_.markers.push_back(warning_zone);
+    visualization_msgs::msg::Marker danger_zone = warning_zone;
+    danger_zone.id = 1;
+    danger_zone.scale.x = danger_distance_ * 2;
+    danger_zone.scale.y = danger_distance_ * 2;
+    danger_zone.color.r = 1.0;
+    danger_zone.color.g = 0.0;
+    danger_zone.color.b = 0.0;
+    danger_zone.color.a = 0.5;
+    danger_zone.pose.position.z = 0.001;
+    zones_.markers.push_back(danger_zone);
   }
 
 private:
   double warning_distance_, danger_distance_;
+  bool is_first_msg_;
   State state_, prev_state_;
+  visualization_msgs::msg::MarkerArray zones_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr safety_stop_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr zones_pub_;
   rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr decrease_speed_client_;
   rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr increase_speed_client_;
 
@@ -73,27 +102,44 @@ private:
       }
     }
 
-    if(state_ != prev_state_)
+    if(state_ != prev_state_ || is_first_msg_)
     {
       std_msgs::msg::Bool is_safety_stop;
       if(state_ == State::WARNING)
       {
         is_safety_stop.data = false;
         decrease_speed_client_->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
+        zones_.markers.at(0).color.a = 1.0;
+        zones_.markers.at(1).color.a = 0.5;
       }
       else if(state_ == State::DANGER)
       {
         is_safety_stop.data = true;
+        zones_.markers.at(0).color.a = 1.0;
+        zones_.markers.at(1).color.a = 1.0;
       }
       else if(state_ == State::FREE)
       {
         is_safety_stop.data = false;
         increase_speed_client_->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
+        zones_.markers.at(0).color.a = 0.5;
+        zones_.markers.at(1).color.a = 0.5;
       }
 
       prev_state_ = state_;
       safety_stop_pub_->publish(is_safety_stop);
     }
+
+    
+    if(is_first_msg_)
+    {
+      for(auto & zone : zones_.markers)
+      {
+        zone.header.frame_id = msg.header.frame_id;
+      }
+      is_first_msg_ = false;
+    }
+    zones_pub_->publish(zones_);
   }
 };
 
