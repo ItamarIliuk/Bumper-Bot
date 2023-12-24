fe4918a2ed3b4f78f4cb5e30a067d4dc2c284c06
AntoBrandi <antonio.brandi@outlook.it>
2023-12-24 15:53:47 +0100

    added python lifecycle node

--- bumperbot_ws/src/bumperbot_localization/src/odometry_motion_model.cpp
+++ bumperbot_ws/src/bumperbot_localization/src/odometry_motion_model.cpp
@@ -145,3 +145,12 @@ void OdometryMotionModel::odomCallback(const nav_msgs::msg::Odometry &odom)
     // Publish samples
     pose_array_pub_->publish(samples_);
 }
+
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<OdometryMotionModel>("odometry_motion_model");
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
